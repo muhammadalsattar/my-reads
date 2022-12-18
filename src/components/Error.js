21c7d1b2ebcfb5f169ab726eaf5c58f6471382bf
muhammadalsattar <mmuhhammadd@outlook.com>
2022-12-18 23:09:38 +0200

    Setup error component

--- src/components/Error.js
+++ src/components/Error.js
@@ -1,8 +1,20 @@
-import React from "react";
+import { Flex, Title } from "@mantine/core";
+import { Link } from "react-router-dom";
 
 
 const Error = ()=>(
-    <div>Error</div>
+    <>
+        <Flex
+        gap="xl"
+        justify="center"
+        align="center"
+        direction="column"
+        wrap="wrap"
+        >
+            <Title order={1}>404</Title>
+            <Title order={4}>Page not found. <Link to="/">Go Back?</Link></Title>
+        </Flex>
+    </>
 )
 
 export default Error;
